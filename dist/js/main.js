68acbbc43bc0d153d896a0c0db8e873b1a5d2a4c
SidVZ <sidvz@protonmail.com>
2022-01-31 01:55:58 +0530

    Add scroll sections active-link feature

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,23 @@
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+    sectionTop = current.offsetTop - 58,
+    sectionId = current.getAttribute('id');
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+    } 
+    else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive);
+
+// Swiper JS
 const swiper = new Swiper('.projects__container', {
     // Optional parameters
     cssMode: true,

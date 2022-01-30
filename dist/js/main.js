4fa80c429575842f7b1fa6385c7f355f0f45947d
SidVZ <sidvz@protonmail.com>
2022-01-30 22:22:23 +0530

    Add projects section along with swiper component

---
+++ dist/js/main.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.projects__container', {
+    // Optional parameters
+    cssMode: true,
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
